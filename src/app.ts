8e17cb4e17876e580355af633242963956584847
yuki-noguchi <y.n.10191@gmail.com>
2023-09-12 23:46:21 +0900

    調整中

--- src/app.ts
+++ src/app.ts
@@ -28,19 +28,22 @@ app.post(
   '/webhook', //
   middleware(lineConfig), //
   wrap(async (req, res) => {
-    await req.body.events.forEach(async (event: WebhookEvent) => {
-      if (isTextMessageEvent(event)) {
-        await handleTextMessageEvent(event);
-      }
-      if (isFollowEvent(event)) {
-        await handleFollowEvent(event);
-      }
-      if (isPostbackEvent(event)) {
-        await handlePostbackEvent(event);
-      }
-    });
-
-    res.status(200).end();
+    req.body.events
+      .forEach(async (event: WebhookEvent) => {
+        if (isTextMessageEvent(event)) {
+          await handleTextMessageEvent(event);
+        }
+        if (isFollowEvent(event)) {
+          await handleFollowEvent(event);
+        }
+        if (isPostbackEvent(event)) {
+          await handlePostbackEvent(event);
+        }
+      })
+      .then(() => {
+        console.log('1');
+        res.status(200).end();
+      });
   }),
 );
 
