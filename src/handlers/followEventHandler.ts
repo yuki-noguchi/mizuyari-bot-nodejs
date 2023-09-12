5e43c6a820c0a46654110600b16e30cb7d20771c
yuki-noguchi <y.n.10191@gmail.com>
2023-09-12 23:30:52 +0900

    調整中

--- src/handlers/followEventHandler.ts
+++ src/handlers/followEventHandler.ts
@@ -5,34 +5,23 @@ import { initialReply } from './replies/initialReply';
 
 export const handleFollowEvent = async (event: FollowEvent) => {
   const userId = event.source.userId;
-  console.log('1');
 
-  await prisma
-    .$transaction(async (tx) => {
-      console.log('2');
-      await tx.user
-        .upsert({
-          create: {
-            id: userId!,
-            status: 'STAND_BY',
-          },
-          update: {
-            status: 'STAND_BY',
-          },
-          where: {
-            id: userId!,
-          },
-        })
-        .catch((e) => console.error(e));
-    })
-    .catch((e) => console.error(e));
-  console.log('3');
-  await lineClient
-    .replyMessage(
-      event.replyToken,
-      initialReply('フォローありがとうございます。', '以下のメニューから操作を', '開始してください。'),
-    )
-    .then((r) => console.log(r))
-    .catch((e) => console.error(e));
-  console.log('4');
+  await prisma.$transaction(async (tx) => {
+    await tx.user.upsert({
+      create: {
+        id: userId!,
+        status: 'STAND_BY',
+      },
+      update: {
+        status: 'STAND_BY',
+      },
+      where: {
+        id: userId!,
+      },
+    });
+  });
+  lineClient.replyMessage(
+    event.replyToken,
+    initialReply('フォローありがとうございます。', '以下のメニューから操作を', '開始してください。'),
+  );
 };
