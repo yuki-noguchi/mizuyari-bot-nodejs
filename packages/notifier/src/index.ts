7bec4bdf273a25c464ec72de7e599516c0e0f764
yuki-noguchi <y.n.10191@gmail.com>
2023-09-19 23:00:49 +0900

    通知を追加

---
+++ packages/notifier/src/index.ts
@@ -0,0 +1,50 @@
+import { config } from 'dotenv';
+config();
+
+// --
+
+import { lineClient, prisma } from '@mizuyari-bot-nodejs/common';
+import { addDays, endOfToday, format, startOfToday } from 'date-fns';
+
+const main = async () =>
+  await prisma.$transaction(async (tx) => {
+    const waterings = await tx.watering.findMany({
+      include: {
+        user: true,
+      },
+      where: {
+        nextDateTime: {
+          gte: startOfToday(),
+          lte: endOfToday(),
+        },
+        frequencyInDays: {
+          not: null,
+        },
+      },
+    });
+
+    await Promise.all(
+      waterings.map(async (watering) => {
+        const nextDateTime = addDays(startOfToday(), watering.frequencyInDays!);
+
+        await tx.watering.update({
+          data: {
+            nextDateTime: nextDateTime,
+          },
+          where: {
+            id: watering.id,
+          },
+        });
+
+        return await lineClient.pushMessage(watering.userId, {
+          type: 'text',
+          text: `${watering.plantName}の水やり日です。次は${format(nextDateTime, 'yyyy年MM月dd日')}です。`,
+        });
+      }),
+    );
+  });
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
